66d6a936760af99ad45ca9d30651c89e6fe853d0
Paloma Bonatte de Almeida <palomabernardo.pbda@gmail.com>
2023-06-28 16:09:14 -0300

    JavaScript formulario contato

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-// Código JavaScript para abrir uma nova janela com campos de formulário para o Fale Conosco
-document.addEventListener("DOMContentLoaded", function() {
-    var faleConoscoLink = document.querySelector("a[href='faleconosco.html']");
-    faleConoscoLink.addEventListener("click", function(event) {
-      event.preventDefault();
-      window.open("faleconosco.html", "_blank", "width=400,height=300");
-    });
+window.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('form');
+  const successMessage = document.getElementById('success-message');
+  const errorMessage = document.getElementById('error-message');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Verificar se todos os campos estão preenchidos
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const telefone = document.getElementById('telefone').value;
+    const mensagem = document.getElementById('mensagem').value;
+
+    if (!nome || !email || !telefone || !mensagem) {
+      // Mostrar mensagem de erro se algum campo estiver vazio
+      errorMessage.textContent = 'Por favor, preencha todos os campos.';
+      successMessage.style.display = 'none';
+    } else {
+      // Limpar mensagens de erro e sucesso
+      errorMessage.textContent = '';
+      successMessage.textContent = '';
+
+      
+      
+      setTimeout(function() {
+        successMessage.textContent = 'Mensagem enviada com sucesso!';
+        successMessage.style.display = 'block';
+      }, 2000);
+    }
   });
-  
+});
+
